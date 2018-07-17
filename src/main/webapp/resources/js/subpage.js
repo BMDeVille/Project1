c1b405df2464d78accfa3845d2d623ebb5e6c55d
Brandon DeVille <bmdnt4@mst.edu>
2018-07-17 15:32:35 -0400

    Remove all

--- src/main/webapp/resources/js/subpage.js
+++
@@ -1,9 +0,0 @@
-/Reimbursement_Application/document.addEventListener("DOMContentLoaded", redirect, false);
-
-function redirect(){
-	if((window.location.href != 'http://localhost:9001/Reimbursement_Application/create-ticket.do') &&
-			(window.location.href != 'http://localhost:9001/Reimbursement_Application/create-employee.do')){
-		var f = document.getElementById('redirect');
-		f.submit();
-	}
-}
