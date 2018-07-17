e683fb3a048eeb3aa1cf50d55a2163545e95a1fd
Brandon DeVille <bmdnt4@mst.edu>
2018-07-17 17:27:00 -0400

    Update request helper

--- Reimbursement_Application/src/main/java/reimbursement/controller/RequestHelper.java
+++ Reimbursement_Application/src/main/java/reimbursement/controller/RequestHelper.java
@@ -5,35 +5,35 @@ import javax.servlet.http.HttpServletRequest;
 public class RequestHelper {
 	public static Object process(HttpServletRequest request) {
 		switch(request.getRequestURI()) {
-			case "/Reimbursement_Application/login.do":
+			case "./login.do":
 				return LoginController.login(request);
-			case "/Reimbursement_Application/sign-out.do":
+			case "./sign-out.do":
 				return LoginController.logout(request);
-			case "/Reimbursement_Application/redirect-home.do":
+			case "./redirect-home.do":
 				return NavigationController.sendHome(request);
-			case "/Reimbursement_Application/create-ticket.do":
+			case "./create-ticket.do":
 				return NavigationController.ticketCreation(request);
-			case "/Reimbursement_Application/create-employee.do":
+			case "./create-employee.do":
 				return NavigationController.employeeCreation(request);
-			case "/Reimbursement_Application/get-user.do":
+			case "./get-user.do":
 				return UserController.getUserProfile(request);
-			case "/Reimbursement_Application/get-users.do":
+			case "./get-users.do":
 				return UserController.getAllUsers(request);
-			case "/Reimbursement_Application/add-user.do":
+			case "./add-user.do":
 				return UserController.addUser(request);
-			case "/Reimbursement_Application/get-tickets.do":
+			case "./get-tickets.do":
 				return ReimbursementController.getAllTickets(request);
-			case "/Reimbursement_Application/get-pending-tickets.do":
+			case "./get-pending-tickets.do":
 				return ReimbursementController.getPendingTickets(request);
-			case "/Reimbursement_Application/get-user-tickets.do":
+			case "./get-user-tickets.do":
 				return ReimbursementController.getUserTickets(request);
-			case "/Reimbursement_Application/get-approved-tickets.do":
+			case "./get-approved-tickets.do":
 				return ReimbursementController.getApprovedTickets(request);
-			case "/Reimbursement_Application/get-denied-tickets.do":
+			case "./get-denied-tickets.do":
 				return ReimbursementController.getDeniedTickets(request);
-			case "/Reimbursement_Application/add-ticket.do":
+			case "./add-ticket.do":
 				return ReimbursementController.requestTicket(request);
-			case "/Reimbursement_Application/audit-ticket.do":
+			case "./audit-ticket.do":
 				return ReimbursementController.auditTicket(request);
 			default:
 				return "";
