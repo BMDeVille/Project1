efc838f4d7dde037c8cbfb54a362ab20f8100cde
Brandon DeVille <bmdnt4@mst.edu>
2018-07-17 17:32:51 -0400

    Update Servlet for logging

--- Reimbursement_Application/src/main/java/reimbursement/servlets/ReimbursementServlet.java
+++ Reimbursement_Application/src/main/java/reimbursement/servlets/ReimbursementServlet.java
@@ -7,9 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import reimbursement.controller.RequestHelper;
+import reimbursement.dao.ReimbursementDAOImpl;
 
 public class ReimbursementServlet extends HttpServlet {
 
@@ -17,13 +20,16 @@ public class ReimbursementServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	final static Logger logger = Logger.getLogger(ReimbursementDAOImpl.class);
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		logger.info("URI = " + req.getRequestURI());
 		resp.setContentType("application/json");
 		resp.getWriter().write(new ObjectMapper().writeValueAsString(RequestHelper.process(req)));
 	}
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		logger.info("URI = " + req.getRequestURI());
 		req.getRequestDispatcher((String)RequestHelper.process(req)).forward(req, resp);
 	}
 }
